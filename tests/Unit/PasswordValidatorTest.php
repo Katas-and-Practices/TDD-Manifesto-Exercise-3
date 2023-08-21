8daf8766c9daf33a66c3d51251f29dc983facad7
Spaceman1994 <farzin1509@gmail.com>
2023-08-21 13:09:21 +0330

    Replace strings in dataProviders with consts

--- tests/Unit/PasswordValidatorTest.php
+++ tests/Unit/PasswordValidatorTest.php
@@ -9,6 +9,11 @@ use PHPUnit\Framework\TestCase;
 
 class PasswordValidatorTest extends TestCase
 {
+    protected const ERROR_MIN_8_CHAR_LENGTH = 'Password must be at least 8 characters long';
+    protected const ERROR_MIN_2_NUMBER = 'Password must contain at least 2 numbers';
+    protected const ERROR_MIN_1_CAPITAL = 'Password must contain at least one capital letter';
+    protected const ERROR_MIN_1_SPECIAL = 'Password must contain at least one special character';
+
     public PasswordValidator $passwordValidator;
 
     public function setUp(): void
@@ -39,31 +44,31 @@ class PasswordValidatorTest extends TestCase
     public static function shouldReturnMultipleErrorMessagesGivenVariousCombinationsOfInvalidInputsDataProvider(): array
     {
         return [
-            ['', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one capital letter\nPassword must contain at least one special character"],
-            ['1', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one capital letter\nPassword must contain at least one special character"],
-            ['acd', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one capital letter\nPassword must contain at least one special character"],
-            ['A', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['52', "Password must be at least 8 characters long\nPassword must contain at least one capital letter\nPassword must contain at least one special character"],
-            [')', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one capital letter"],
-            ['Bacd', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['a1a5', "Password must be at least 8 characters long\nPassword must contain at least one capital letter\nPassword must contain at least one special character"],
-            ['abcD3ef', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['abc3efabc', "Password must contain at least 2 numbers\nPassword must contain at least one capital letter\nPassword must contain at least one special character"],
-            ['B_c?', "Password must be at least 8 characters long\nPassword must contain at least 2 numbers"],
-            ['ab|3efabc', "Password must contain at least 2 numbers\nPassword must contain at least one capital letter"],
-            ['a7Bc3', "Password must be at least 8 characters long\nPassword must contain at least one special character"],
-            ['AB34Z67', "Password must be at least 8 characters long\nPassword must contain at least one special character"],
-            ['abcAefgh', "Password must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['abcdBfg1', "Password must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['abcdefg1H', "Password must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['abcHefghijK2', "Password must contain at least 2 numbers\nPassword must contain at least one special character"],
-            ['abcdefg15', "Password must contain at least one capital letter\nPassword must contain at least one special character"],
-            ['a7Bc3\'', "Password must be at least 8 characters long"],
-            ['!bcdefg1H', "Password must contain at least 2 numbers"],
-            ['ab+ef7hijd2', "Password must contain at least one capital letter"],
-            ['ab6ef7_ijd2', "Password must contain at least one capital letter"],
-            ['1234567A', 'Password must contain at least one special character'],
-            ['ab2c2fAh24gd', 'Password must contain at least one special character'],
+            ['', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['1', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['acd', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['A', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['52', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            [')', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_CAPITAL],
+            ['Bacd', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['a1a5', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abcD3ef', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abc3efabc', self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['B_c?', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_2_NUMBER],
+            ['ab|3efabc', self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_CAPITAL],
+            ['a7Bc3', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['AB34Z67', self::ERROR_MIN_8_CHAR_LENGTH . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abcAefgh', self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abcdBfg1', self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abcdefg1H', self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abcHefghijK2', self::ERROR_MIN_2_NUMBER . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['abcdefg15', self::ERROR_MIN_1_CAPITAL . "\n" . self::ERROR_MIN_1_SPECIAL],
+            ['a7Bc3\'', self::ERROR_MIN_8_CHAR_LENGTH],
+            ['!bcdefg1H', self::ERROR_MIN_2_NUMBER],
+            ['ab+ef7hijd2', self::ERROR_MIN_1_CAPITAL],
+            ['ab6ef7_ijd2', self::ERROR_MIN_1_CAPITAL],
+            ['1234567A', self::ERROR_MIN_1_SPECIAL],
+            ['ab2c2fAh24gd', self::ERROR_MIN_1_SPECIAL],
         ];
     }
 
